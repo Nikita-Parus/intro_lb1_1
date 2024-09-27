6ee9f5367337a5de5a87fbdd66f4c30aca832671
Nikita-Parus <cheems.nz@gmail.com>
2024-09-27 14:44:43 +0300

    Добавьте файлы проекта.

---
+++ intro_lb1_1/intro_lb1_1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0,".1251");
+	int  S, P, a, b;
+	double sum, dif, mul, div;
+	
+	cout << "Введiть А та В: ";
+	cin >> a >> b;
+	S = a * b;
+	P = 2 * (a + b);
+
+	printf("S = %d P = %d\n",S,P);
+
+	sum = (double)S + P;
+	dif = (double)S - P;
+	mul = (double)S * P;
+	div = (double)S / P;
+	
+	printf("Sum: %f Dif: %f Mul: %f Div: %f\n", sum, dif, mul, div);
+
+	cout << "Адреси: \n";
+	printf("S: %p P: %p a: %p b: %p\n", &S, &P, &a, &b);
+	printf("Sum: %p Dif: %p Mul: %p Div: %p\n", &sum, &dif, &mul, &div);
+}
